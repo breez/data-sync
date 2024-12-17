420a90b5fd8c3cc3e2644cc8d1249e04f659d3c3
yse <70684173+hydra-yse@users.noreply.github.com>
2024-12-17 23:25:48 +0100

    feat: add API key check (#9)
    * feat: add API key check
    
    * fix: make CA certificate optional
    
    * fix: add `db` dir to gitignore

--- config/config.go
+++ config/config.go
@@ -1,19 +1,48 @@
 package config
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"log"
+	"os"
+
 	"github.com/Netflix/go-env"
 )
 
 type Config struct {
-	GrpcListenAddress string `env:"GRPC_LISTEN_ADDRESS,default=0.0.0.0:8080"`
-	SQLiteDirPath     string `env:"SQLITE_DIR_PATH,default=db"`
-	PgDatabaseUrl     string `env:"DATABASE_URL"`
+	GrpcListenAddress string  `env:"GRPC_LISTEN_ADDRESS,default=0.0.0.0:8080"`
+	SQLiteDirPath     string  `env:"SQLITE_DIR_PATH,default=db"`
+	PgDatabaseUrl     string  `env:"DATABASE_URL"`
+	CACertPath        *string `env:"CA_CERT_PATH"`
+	CACert            *x509.Certificate
+}
+
+func initializeCACert(certPath string) *x509.Certificate {
+	certData, err := os.ReadFile(certPath)
+	if err != nil {
+		log.Fatal("CA certificate not found")
+	}
+
+	CACertBlock, _ := pem.Decode(certData)
+	if CACertBlock == nil {
+		log.Fatal("CA certificate is invalid")
+	}
+
+	CACert, err := x509.ParseCertificate(CACertBlock.Bytes)
+	if err != nil {
+		log.Fatal("Could not parse CA cert:", err)
+	}
+
+	return CACert
 }
 
 func NewConfig() (*Config, error) {
 	var config Config
 	if _, err := env.UnmarshalFromEnviron(&config); err != nil {
 		return nil, err
+	}
+	if config.CACertPath != nil {
+		config.CACert = initializeCACert(*config.CACertPath)
 	}
 
 	return &config, nil
