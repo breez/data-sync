702add006813ddad85c08caab011e1d1909eae36
yse <hydra_yse@proton.me>
2024-12-17 23:23:00 +0100

    fix: update proto to new definition

--- store/postgres/pg.go
+++ store/postgres/pg.go
@@ -57,7 +57,7 @@ func NewPGSyncStorage(databaseURL string) (*PgSyncStorage, error) {
 	return &PgSyncStorage{db: pgxPool}, nil
 }
 
-func (s *PgSyncStorage) SetRecord(ctx context.Context, userID, id string, data []byte, existingRevision int64) (int64, error) {
+func (s *PgSyncStorage) SetRecord(ctx context.Context, userID, id string, data []byte, existingRevision uint64, schemaVersion string) (uint64, error) {
 	tx, err := s.db.BeginTx(ctx, pgx.TxOptions{
 		IsoLevel: pgx.Serializable,
 	})
@@ -67,7 +67,7 @@ func (s *PgSyncStorage) SetRecord(ctx context.Context, userID, id string, data [
 	defer tx.Rollback(context.Background())
 
 	// check that the existing revision is the same as the one we expect
-	var revision int64
+	var revision uint64
 	err = tx.QueryRow(ctx, "SELECT revision FROM records WHERE user_id = $1 AND id = $2", userID, id).Scan(&revision)
 	if err != pgx.ErrNoRows {
 		if err != nil {
@@ -79,25 +79,48 @@ func (s *PgSyncStorage) SetRecord(ctx context.Context, userID, id string, data [
 	}
 
 	// get the store's last revision
-	var newRevision int64
-	err = tx.QueryRow(ctx, "INSERT INTO user_revisions (user_id, revision) VALUES ($1, 1) ON CONFLICT(user_id, revision) DO UPDATE SET revision=user_revisions.revision + 1 RETURNING revision", userID).Scan(&newRevision)
-	if err != nil {
-		return 0, fmt.Errorf("failed to set store's latest revision: %w", err)
+	var newRevision uint64 = 0
+	err = tx.QueryRow(ctx, "SELECT revision FROM user_revisions WHERE user_id = $1", userID).Scan(&newRevision)
+	if err != pgx.ErrNoRows {
+		if err != nil {
+			return 0, fmt.Errorf("failed to get store's latest revision: %w", err)
+		}
+	}
+	if newRevision == 0 {
+		err := tx.QueryRow(ctx, "INSERT INTO user_revisions (user_id, revision) VALUES ($1, $2) RETURNING revision", userID, 1).Scan(&newRevision)
+		if err != nil {
+			return 0, fmt.Errorf("failed to insert store's revision: %w", err)
+		}
+	} else {
+		err := tx.QueryRow(ctx, "UPDATE user_revisions SET revision = revision + 1 WHERE user_id = $1 RETURNING revision", userID).Scan(&newRevision)
+		if err != nil {
+			return 0, fmt.Errorf("failed to update store's revision: %w", err)
+		}
 	}
 
-	_, err = tx.Exec(ctx, "INSERT INTO records (user_id, id, data, revision) VALUES ($1, $2, $3, $4) ON CONFLICT (user_id, id) DO UPDATE SET data=EXCLUDED.data, revision=EXCLUDED.revision RETURNING revision", userID, id, data, newRevision)
+	_, err = tx.Exec(ctx, `
+		INSERT INTO records (user_id, id, data, revision, schema_version) 
+		VALUES ($1, $2, $3, $4, $5) 
+		ON CONFLICT (user_id, id) DO UPDATE SET 
+			data=EXCLUDED.data, 
+			revision=EXCLUDED.revision, 
+			schema_version=EXCLUDED.schema_version 
+		RETURNING revision`,
+		userID, id, data, newRevision, schemaVersion,
+	)
 	if err != nil {
 		return 0, fmt.Errorf("failed to insert record: %w", err)
 	}
+
 	if err := tx.Commit(ctx); err != nil {
 		return 0, fmt.Errorf("failed to commit transaction: %w", err)
 	}
 	return newRevision, nil
 }
 
-func (s *PgSyncStorage) ListChanges(ctx context.Context, userID string, sinceRevision int64) ([]store.StoredRecord, error) {
+func (s *PgSyncStorage) ListChanges(ctx context.Context, userID string, sinceRevision uint64) ([]store.StoredRecord, error) {
 
-	rows, err := s.db.Query(ctx, "SELECT id, data, revision FROM records WHERE user_id = $1 AND revision > $2", userID, sinceRevision)
+	rows, err := s.db.Query(ctx, "SELECT id, data, revision, schema_version FROM records WHERE user_id = $1 AND revision > $2", userID, sinceRevision)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query records: %w", err)
 	}
@@ -106,7 +129,7 @@ func (s *PgSyncStorage) ListChanges(ctx context.Context, userID string, sinceRev
 	records := make([]store.StoredRecord, 0)
 	for rows.Next() {
 		record := store.StoredRecord{}
-		err = rows.Scan(&record.Id, &record.Data, &record.Revision)
+		err = rows.Scan(&record.Id, &record.Data, &record.Revision, &record.SchemaVersion)
 		if err != nil {
 			return nil, fmt.Errorf("failed to scan record: %w", err)
 		}
