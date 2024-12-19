489cab6a61322d0a15791a6a56f5da93262cdb6a
yse <hydra_yse@proton.me>
2024-12-19 03:19:54 +0100

    fix: pass certificate as env variable

--- config/config.go
+++ config/config.go
@@ -4,26 +4,16 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"log"
-	"os"
 
 	"github.com/Netflix/go-env"
 )
 
-type Config struct {
-	GrpcListenAddress string  `env:"GRPC_LISTEN_ADDRESS,default=0.0.0.0:8080"`
-	SQLiteDirPath     string  `env:"SQLITE_DIR_PATH,default=db"`
-	PgDatabaseUrl     string  `env:"DATABASE_URL"`
-	CACertPath        *string `env:"CA_CERT_PATH"`
-	CACert            *x509.Certificate
+type Certificate struct {
+	Raw *x509.Certificate
 }
 
-func initializeCACert(certPath string) *x509.Certificate {
-	certData, err := os.ReadFile(certPath)
-	if err != nil {
-		log.Fatal("CA certificate not found")
-	}
-
-	CACertBlock, _ := pem.Decode(certData)
+func (c *Certificate) UnmarshalEnvironmentValue(data string) error {
+	CACertBlock, _ := pem.Decode([]byte(data))
 	if CACertBlock == nil {
 		log.Fatal("CA certificate is invalid")
 	}
@@ -33,16 +23,22 @@ func initializeCACert(certPath string) *x509.Certificate {
 		log.Fatal("Could not parse CA cert:", err)
 	}
 
-	return CACert
+	c.Raw = CACert
+
+	return nil
+}
+
+type Config struct {
+	GrpcListenAddress string       `env:"GRPC_LISTEN_ADDRESS,default=0.0.0.0:8080"`
+	SQLiteDirPath     string       `env:"SQLITE_DIR_PATH,default=db"`
+	PgDatabaseUrl     string       `env:"DATABASE_URL"`
+	CACert            *Certificate `env:"CA_CERT"`
 }
 
 func NewConfig() (*Config, error) {
 	var config Config
 	if _, err := env.UnmarshalFromEnviron(&config); err != nil {
 		return nil, err
-	}
-	if config.CACertPath != nil {
-		config.CACert = initializeCACert(*config.CACertPath)
 	}
 
 	return &config, nil
